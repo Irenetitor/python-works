470d2805bc8a07d057d69348358353579d568966
Irene <martincampoirene@gmail.com>
2026-01-30 20:58:59 +0000

    code clean-up

--- Backend-with-Python-and-FastAPI/db/client.py
+++ Backend-with-Python-and-FastAPI/db/client.py
@@ -6,7 +6,5 @@ import os
 db_client = MongoClient().local
 
 #Remote database
-#db_client = MongoClient("mongodb+srv://martincampoirene_db_user:28BYPkofzlM14nLu@cluster0.tpqq5io.mongodb.net/?appName=Cluster0").test
-
 # MONGO_URI = os.getenv("MONGO_URI")
 # db_client = MongoClient(MONGO_URI).test
