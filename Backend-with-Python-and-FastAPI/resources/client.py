84c6163e68b2055df232470fcd25c2ed3440e602
Irene <martincampoirene@gmail.com>
2026-01-30 13:06:11 +0000

    Add MongoDB integration

---
+++ Backend-with-Python-and-FastAPI/resources/client.py
@@ -0,0 +1,9 @@
+# Descarga versión community: https://www.mongodb.com/try/download
+# Instalación:https://www.mongodb.com/docs/manual/tutorial
+# Módulo conexión MongoDB: pip install pymongo
+# Ejecución: sudo mongod --dbpath "/path/a/la/base/de/datos/"
+# Conexión: mongodb://localhost
+
+from pymongo import MongoClient
+
+db_client = MongoClient().local
